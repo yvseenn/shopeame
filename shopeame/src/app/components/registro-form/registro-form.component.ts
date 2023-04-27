7b7a3beee3ada52772207ae64ae0ce691b8cc02d
eavellanet <eavellanet12@gmail.com>
2023-04-27 11:11:14 +0200

    forms de login y registro

---
+++ shopeame/src/app/components/registro-form/registro-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-form',
+  templateUrl: './registro-form.component.html',
+  styleUrls: ['./registro-form.component.scss']
+})
+export class RegistroFormComponent {
+
+}
